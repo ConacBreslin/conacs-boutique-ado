13fe98a86e237465cc9b2be3a07c2b6327d03011
ConacBreslin <conacbreslin@sky.com>
2021-10-25 14:02:21 +0000

    deploy to Heroku and add data to postgres

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,11 +118,15 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://todjfroeouwedb:197867078d077cb48f5b48add7424febf0f7ef2db8e5c91b1fae0358acaba4c4@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/d4ehn7jdkfv6i5')
 }
 
 
