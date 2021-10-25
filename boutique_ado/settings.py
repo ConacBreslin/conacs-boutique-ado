ad13845447fcba92e4e9f13f2d7428b62d503d7a
ConacBreslin <conacbreslin@sky.com>
2021-10-25 14:18:00 +0000

    Deplot app to Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -118,16 +118,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES = {
-    'default': dj_database_url.parse('postgres://todjfroeouwedb:197867078d077cb48f5b48add7424febf0f7ef2db8e5c91b1fae0358acaba4c4@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/d4ehn7jdkfv6i5')
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
